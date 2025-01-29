b555b66b1ddd5dc57c194ba38ad55007839ddd04
deviid123 <devidkoch321@gmail.com>
2025-01-29 08:05:57 +0000

    add

--- src/services/state/api/departmentCreationApi.ts
+++ src/services/state/api/departmentCreationApi.ts
@@ -4,7 +4,7 @@ import { ENDPOINTS } from "../../../api-endpoints/endpoints";
 
 export const getCreatedDepartments = async (): Promise<any> => {
   const response = await axiosInstance.post(ENDPOINTS.getCreatedDepartments);
-  return response.data.data;
+  return response.data.data.departmentData;
 };
 
 export const createDepartmentLogin = async (data:any): Promise<any> => {
